89ee07a3773c1b6d1f7936bf0cbf69fdb999e59a
Marissa Zhong <marissazhong@Marissas-MacBook-Pro.local>
2018-08-10 22:32:15 -0400

    seed data and images for recipes index

--- client/src/components/recipes/RecipeList.js
+++ client/src/components/recipes/RecipeList.js
@@ -4,12 +4,16 @@ import RecipeShow from './RecipeShow';
 import defaultImage from '../../images/recipes/defaultRecipe.jpg';
 import Images from '../../images/ImportImages';
 
+function slug(string) {
+  return string.toLowerCase().replace(/\s/g,'-');
+}
+
 class RecipeList extends React.Component {
   render() {
     const recipes = this.props.recipes;
     const renderRecipes = Object.keys(recipes).map(recipeID =>
         <Link key={recipeID} className="card" to={`recipes/${recipeID}`} style={{ width: '240px', margin: '20px' }}>
-          <img className="card-img-top" src={Images[`${recipeID}.jpg`]} alt={recipes[recipeID].name} />
+          <img className="card-img-top" src={Images[`${slug(recipes[recipeID].name)}.jpg`]} alt={recipes[recipeID].name} />
           <div class="card-body">
           <h6 class="card-title">{recipes[recipeID].name}</h6>
           </div>
