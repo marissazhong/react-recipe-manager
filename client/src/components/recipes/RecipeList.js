32f29904c5c1f4768c5f93e868c1370af1d5aebe
Marissa Zhong <marissazhong@Marissas-MacBook-Pro.local>
2018-08-16 09:50:03 -0400

    add image loader to recipe list component

--- client/src/components/recipes/RecipeList.js
+++ client/src/components/recipes/RecipeList.js
@@ -6,12 +6,20 @@ function slug(string) {
   return string.toLowerCase().replace(/\s/g,'-');
 }
 
+function loadImage(recipe) {
+  if (Images[slug(recipe.name) + '.jpg']) {
+    return slug(recipe.name) + '.jpg'
+  } else {
+    return 'defaultRecipe.jpg'
+  }
+}
+
 class RecipeList extends React.Component {
   render() {
     const recipes = this.props.recipes;
     const renderRecipes = Object.keys(recipes).map(recipeID =>
         <Link key={recipeID} className="card" to={`recipes/${recipeID}`} style={{ width: '240px', margin: '20px' }}>
-          <img className="card-img-top" src={Images[`${slug(recipes[recipeID].name)}.jpg`]} alt={recipes[recipeID].name} />
+          <img className="card-img-top" src={Images[loadImage(recipes[recipeID])]} alt={recipes[recipeID].name} />
           <div className="card-body">
           <h6 className="card-title">{recipes[recipeID].name}</h6>
           </div>
