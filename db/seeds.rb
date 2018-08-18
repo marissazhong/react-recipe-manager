7ed8aa879c6f45be535583ee0e6b44fc7dc8db38
Marissa Zhong <marissazhong@Marissas-MacBook-Pro.local>
2018-08-18 17:17:23 -0400

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -174,7 +174,172 @@ recipe10 = {name: "Challah Bread", prep_time: 30, cook_time: 40, ingredients_rec
     "3": "Beat the remaining egg and brush a generous amount over each braid.",
     "4": "Bake at 375 degrees F (190 degrees C) for about 40 minutes. Bread should have a nice hollow sound when thumped on the bottom. Cool on a rack for at least one hour before slicing."}}
 
-recipes = [recipe1, recipe2, recipe3, recipe4, recipe5, recipe6, recipe7, recipe8, recipe9, recipe10]
+recipe11 = {name: "Carrot Cake", prep_time: 20, cook_time: 60, ingredients_recipes_attributes: {
+    "0": {name: "egg", quantity: "3 ct"},
+    "1": {name: "buttermilk", quantity: "3/4 cup"},
+    "2": {name: "vegetable oil", quantity: "3/4 cup"},
+    "3": {name: "sugar", quantity: "1-1/2 cups"},
+    "4": {name: "vanilla extract", quantity: "2 tsp"},
+    "5": {name: "cinnamon", quantity: "2 tsp"},
+    "6": {name: "salt", quantity: "1/4 tsp"},
+    "7": {name: "flour", quantity: "2 cups"},
+    "8": {name: "baking soda", quantity: "2 tsp"},
+    "9": {name: "shredded carrots", quantity: "2 cups"},
+    "10": {name: "flaked coconut", quantity: "2 cups"},
+    "11": {name: "walnuts", quantity: "1 cup"},
+    "12": {name: "crushed pineapple", quantity: "8 oz"},
+    "13": {name: "raisins", quantity: "1 cup"}
+}, directions: {
+    "0": "Preheat oven to 350 degrees F (175 degrees C). Grease and flour an 8x12 inch pan.",
+    "1": "In a medium bowl, sift together flour, baking soda, salt and cinnamon. Set aside.",
+    "2": "In a large bowl, combine eggs, buttermilk, oil, sugar and vanilla. Mix well. Add flour mixture and mix well.",
+    "3": "In a medium bowl, combine shredded carrots, coconut, walnuts, pineapple and raisins.",
+    "4": "Using a large wooden spoon or a very heavy whisk, add carrot mixture to batter and fold in well.",
+    "5": "Pour into prepared 8x12 inch pan, and bake at 350 degrees F (175 degrees C) for 1 hour. Check with toothpick.",
+    "6": "Allow to cool for at least 20 minutes before serving."}}
+
+recipe12 = {name: "Peanut Butter Cookies", prep_time: 15, cook_time: 10, ingredients_recipes_attributes: {
+    "0": {name: "butter", quantity: "1 cup"},
+    "1": {name: "peanut butter", quantity: "1 cup"},
+    "2": {name: "white sugar", quantity: "1 cup"},
+    "3": {name: "brown sugar", quantity: "1 cup"},
+    "4": {name: "egg", quantity: "2 ct"},
+    "5": {name: "flour", quantity: "2-1/2 cups"},
+    "6": {name: "baking powder", quantity: "1 tsp"},
+    "7": {name: "salt", quantity: "1/2 tsp"},
+    "8": {name: "baking soda", quantity: "1-1/2 tsp"}
+}, directions: {
+    "0": "Cream butter, peanut butter, and sugars together in a bowl; beat in eggs.",
+    "1": "In a separate bowl, sift flour, baking powder, baking soda, and salt; stir into butter mixture. Put dough in refrigerator for 1 hour.",
+    "2": "Roll dough into 1 inch balls and put on baking sheets. Flatten each ball with a fork, making a crisscross pattern. Bake in a preheated 375 degrees F oven for about 10 minutes or until cookies begin to brown."}}
+
+recipe13 = {name: "Lemon Bars", prep_time: 15, cook_time: 40, ingredients_recipes_attributes: {
+    "0": {name: "butter", quantity: "1 cup"},
+    "1": {name: "sugar", quantity: "1/2 cup"},
+    "2": {name: "flour", quantity: "2 cups"},
+    "3": {name: "egg", quantity: "4 ct"},
+    "4": {name: "sugar", quantity: "1-1/2 cups"},
+    "5": {name: "flour", quantity: "1/4 cup"},
+    "6": {name: "lemon", quantity: "2 ct"}         
+}, directions: {
+    "0": "Preheat oven to 350 degrees F (175 degrees C).",
+    "1": "In a medium bowl, blend together softened butter, 2 cups flour and 1/2 cup sugar. Press into the bottom of an ungreased 9x13 inch pan.",
+    "2": "Bake for 15 to 20 minutes in the preheated oven, or until firm and golden. In another bowl, whisk together the remaining 1 1/2 cups sugar and 1/4 cup flour. Whisk in the eggs and lemon juice. Pour over the baked crust.",
+    "3": "Bake for an additional 20 minutes in the preheated oven. The bars will firm up as they cool. For a festive tray, make another pan using limes instead of lemons and adding a drop of green food coloring to give a very pale green. After both pans have cooled, cut into uniform 2 inch squares and arrange in a checker board fashion."}}
+
+recipe14 = {name: "Sugar Cookies", prep_time: 15, cook_time: 10, ingredients_recipes_attributes: {
+    "0": {name: "flour", quantity: "2-3/4 cups"},
+    "1": {name: "baking soda", quantity: "1 tsp"},
+    "2": {name: "baking powder", quantity: "1/2 tsp"},
+    "3": {name: "butter", quantity: "1 cup"},
+    "4": {name: "sugar", quantity: "1-1/2 cups"},
+    "5": {name: "egg", quantity: "1 ct"},
+    "6": {name: "vanilla extract", quantity: "1 tsp"}            
+}, directions: {
+    "0": "Preheat oven to 375 degrees F (190 degrees C). In a small bowl, stir together flour, baking soda, and baking powder. Set aside.",
+    "1": "In a large bowl, cream together the butter and sugar until smooth. Beat in egg and vanilla. Gradually blend in the dry ingredients. Roll rounded teaspoonfuls of dough into balls, and place onto ungreased cookie sheets.",
+    "2": "Bake 8 to 10 minutes in the preheated oven, or until golden. Let stand on cookie sheet two minutes before removing to cool on wire racks."}}
+
+recipe15 = {name: "Oatmeal Raisin Cookies", prep_time: 15, cook_time: 10, ingredients_recipes_attributes: {
+    "0": {name: "butter", quantity: "3/4 cup"},
+    "1": {name: "white sugar", quantity: "3/4 cup"},
+    "2": {name: "brown sugar", quantity: "3/4 cup"},
+    "3": {name: "egg", quantity: "2 ct"},
+    "4": {name: "vanilla extract", quantity: "1 tsp"},
+    "5": {name: "flour", quantity: "1-1/4 cup"},
+    "6": {name: "baking soda", quantity: "1 tsp"},
+    "7": {name: "cinnamon", quantity: "3/4 tsp"},
+    "8": {name: "salt", quantity: "1/2 tsp"},
+    "9": {name: "oats", quantity: "2-3/4 cups"},            
+    "10": {name: "raisins", quantity: "1 cup"}            
+}, directions: {
+    "0": "Preheat oven to 375 degrees F (190 degrees C).",
+    "1": "In large bowl, cream together butter, white sugar, and brown sugar until smooth. Beat in the eggs and vanilla until fluffy. Stir together flour, baking soda, cinnamon, and salt. Gradually beat into butter mixture. Stir in oats and raisins. Drop by teaspoonfuls onto ungreased cookie sheets.",
+    "2": "Bake 8 to 10 minutes in the preheated oven, or until golden brown. Cool slightly, remove from sheet to wire rack. Cool completely."}}
+
+recipe16 = {name: "Cranberry Pistachio Biscotti", prep_time: 25, cook_time: 45, ingredients_recipes_attributes: {
+    "0": {name: "olive oil", quantity: "1/4 cup"},
+    "1": {name: "sugar", quantity: "3/4 cup"},
+    "2": {name: "vanilla extract", quantity: "2 tsp"},
+    "3": {name: "almond extract", quantity: "1/2 tsp"},
+    "4": {name: "egg", quantity: "2 ct"},
+    "5": {name: "flour", quantity: "1-3/4 cup"},
+    "6": {name: "salt", quantity: "1/4 tsp"},
+    "7": {name: "baking powder", quantity: "1 tsp"},
+    "8": {name: "dried cranberries", quantity: "1/2 cup"},
+    "9": {name: "pistachios", quantity: "1-1/2 cups"}
+}, directions: {
+    "0": "Preheat the oven to 300 degrees F (150 degrees C).",
+    "1": "In a large bowl, mix together oil and sugar until well blended. Mix in the vanilla and almond extracts, then beat in the eggs. Combine flour, salt, and baking powder; gradually stir into egg mixture. Mix in cranberries and nuts by hand.",
+    "2": "Divide dough in half. Form two logs (12x2 inches) on a cookie sheet that has been lined with parchment paper. Dough may be sticky; wet hands with cool water to handle dough more easily.",
+    "3": "Bake for 35 minutes in the preheated oven, or until logs are light brown. Remove from oven, and set aside to cool for 10 minutes. Reduce oven heat to 275 degrees F (135 degrees C).",
+    "4": "Cut logs on diagonal into 3/4 inch thick slices. Lay on sides on parchment covered cookie sheet. Bake approximately 8 to 10 minutes, or until dry; cool."}}
+
+recipe17 = {name: "Russian Tea Cakes", prep_time: 60, cook_time: 45, ingredients_recipes_attributes: {
+    "0": {name: "flour", quantity: "1 cup"},
+    "1": {name: "pecans", quantity: "1 cup"},
+    "2": {name: "butter", quantity: "1/2 cup"},
+    "3": {name: "sugar", quantity: "2 tbs"},
+    "4": {name: "vanilla extract", quantity: "1 tsp"}            
+}, directions: {
+    "0": "Heat oven to 300F. Have ungreased cookies sheet ready.",
+    "1": "Process flour and pecans in a food processor or blender until nuts are very finely chopped.",
+    "2": "Beat butter, sugar and vanilla in a large bowl with electric mixer until pale and fluffy. With mixer on low speed, gradually beat in flour mixture just until blended.",
+    "3": "Roll level tablespoonfuls dough into balls. Place about 1 inch apart on cookie sheet.",
+    "4": "Bake 45 min or until golden. While warm, roll in confectioners' sugar. Place on wire rack to cool completely, then roll in confectioners' sugar again."}}
+
+recipe18 = {name: "Red Velvet Cake", prep_time: 40, cook_time: 20, ingredients_recipes_attributes: {
+    "0": {name: "butter", quantity: "1/2 cup"},
+    "1": {name: "cocoa powder", quantity: "3 tbs"},
+    "2": {name: "sugar", quantity: "1-1/2 cup"},
+    "3": {name: "egg", quantity: "2 ct"},
+    "4": {name: "vanilla extract", quantity: "2 tsp"},
+    "5": {name: "red food coloring", quantity: "2 tbs"},
+    "6": {name: "salt", quantity: "1 tsp"},
+    "7": {name: "baking soda", quantity: "1 tsp"},
+    "8": {name: "flour", quantity: "2-1/2 cups"},
+    "9": {name: "buttermilk", quantity: "1 cup"},
+    "10": {name: "vinegar", quantity: "1 tbs"}
+}, directions: {
+    "0": "Heat oven to 350 degrees. Prepare three 9-inch cake pans by buttering lightly and sprinkling with 1 tablespoon sifted cocoa powder, tapping pans to coat and discarding extra cocoa.",
+    "1": "Cream butter and sugar together. Add eggs one at a time and beat vigorously until each is incorporated. Mix in vanilla.",
+    "2": "In a separate bowl, make a paste of the remaining 2 tablespoons cocoa and the food coloring. Blend into butter mixture.",
+    "3": "Sift together remaining dry ingredients. Alternating in 2 batches each, add dry ingredients and buttermilk to the butter mixture. In the last batch of buttermilk, mix in the vinegar before adding to the batter. Mix until blended.",
+    "4": "Divide batter among 3 pans and bake for about 20 to 25 minutes. Cool on a rack completely. (Can also be made in 2 cake pans.)",
+    "5": "To assemble, remove 1 cake from its pan and place flat side down on a serving platter. Drop about 1 cup of icing onto cake and, using a flat spatula, spread evenly over top. Remove the second cake from its pan. Place flat side down on top of first layer. Use remaining frosting to cover top and sides of cake."}}
+
+recipe19 = {name: "Cheesecake", prep_time: 30, cook_time: 45, ingredients_recipes_attributes: {
+    "0": {name: "graham crackers", quantity: "2 cups"},
+    "1": {name: "cinnamon", quantity: "1/2 tsp"},
+    "2": {name: "butter", quantity: "1/2 cup"},
+    "3": {name: "cream cheese", quantity: "1 lb"},
+    "4": {name: "egg", quantity: "3 ct"},
+    "5": {name: "sugar", quantity: "1 cup"},
+    "6": {name: "sour cream", quantity: "1 pint"},
+    "7": {name: "lemon zest", quantity: "1 ct"},
+    "8": {name: "vanilla extract", quantity: "1/2 tsp"}            
+}, directions: {
+    "0": "Preheat the oven to 325 degrees F.",
+    "1": "In a mixing bowl, combine the ingredients with a fork until evenly moistened. Lightly coat the bottom and sides of an 8-inch springform pan with non-stick cooking spray.",
+    "2": "Pour the crumbs into the pan and, using the bottom of a measuring cup or the smooth bottom of a glass, press the crumbs down into the base and 1-inch up the sides. Refrigerate for 5 minutes.",
+    "3": "In the bowl of an electric mixer, beat the cream cheese on low speed for 1 minute until smooth and free of any lumps. Add the eggs, 1 at a time, and continue to beat slowly until combined. Gradually add sugar and beat until creamy, for 1 to 2 minutes.",
+    "4": "Add sour cream, lemon zest, and vanilla. Periodically scrape down the sides of the bowl and the beaters. The batter should be well-mixed but not over-beaten. Pour the filling into the crust-lined pan and smooth the top with a spatula.",
+    "5": "Set the cheesecake pan on a large piece of aluminum foil and fold up the sides around it. Place the cake pan in a large roasting pan. Pour boiling water into the roasting pan until the water is about halfway up the sides of the cheesecake pan; the foil will keep the water from seeping into the cheesecake. Bake for 45 minutes. The cheesecake should still jiggle (it will firm up after chilling), so be careful not to overcook. Let cool in pan for 30 minutes. Chill in the refrigerator, loosely covered, for at least 4 hours. Loosen the cheesecake from the sides of the pan by running a thin metal spatula around the inside rim. Unmold and transfer to a cake plate."}}
+
+recipe20 = {name: "White Cake", prep_time: 20, cook_time: 20, ingredients_recipes_attributes: {
+    "0": {name: "sugar", quantity: "1 cup"},
+    "1": {name: "butter", quantity: "1/2 cup"},
+    "2": {name: "egg", quantity: "2 ct"},
+    "3": {name: "vanilla extract", quantity: "2 tsp"},
+    "4": {name: "flour", quantity: "1-1/2 cups"},
+    "5": {name: "baking powder", quantity: "1-3/4 tsp"},
+    "6": {name: "milk", quantity: "1/2 cup"}
+}, directions: {
+    "0": "Preheat oven to 350 degrees F (175 degrees C). Grease and flour a 9x9 inch pan or line a muffin pan with paper liners.",
+    "1": "In a medium bowl, cream together the sugar and butter. Beat in the eggs, one at a time, then stir in the vanilla. Combine flour and baking powder, add to the creamed mixture and mix well. Finally stir in the milk until batter is smooth. Pour or spoon batter into the prepared pan.",
+    "2": "Bake for 30 to 40 minutes in the preheated oven. For cupcakes, bake 20 to 25 minutes. Cake is done when it springs back to the touch."}}
+
+recipes = [recipe1, recipe2, recipe3, recipe4, recipe5, recipe6, recipe7, recipe8, recipe9, recipe10, recipe11, recipe12, recipe13, recipe14, recipe15, recipe16, recipe17, recipe18, recipe19, recipe20]
 
 recipes.each do |recipe|
     new_recipe = Recipe.create(name: recipe[:name], prep_time: recipe[:prep_time], cook_time: recipe[:cook_time], user_id: User.find_by(username: "marissa").id)
